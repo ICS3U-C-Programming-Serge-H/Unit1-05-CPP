a828d30b316b8ef2b80a65e78510d4cb3ccd3409
Serge Hamouche <Serge.el-hamouche@stu.ocsb.ca>
2025-02-23 19:54:37 +0000

    commit areaPerRect.cpp

---
+++ areaPerRect.cpp
@@ -0,0 +1,19 @@
+// Copyright (c) 2025 Serge All rights reserved
+//
+
+// Created by Serge Hamouche
+// Created on Feb 2025
+
+// This program calculates the area and perimeter of a rectangle
+// with dimensions 3cm x 2cm
+
+#include <iostream>
+
+int main() {
+    std::cout << "If a rectangle has the dimensions:" << std::endl;
+    std::cout << "3cm x 2cm" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "Area is: " << (3 * 2) << "cm^2" << std::endl;
+    std::cout << "Perimeter is: " << (2 * (3 + 2)) << "cm" << std::endl;
+}
+ 
